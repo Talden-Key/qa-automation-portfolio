d6cfb850c5eecbaacfa6ad41b172cd875948d1e0
Talden-Key <tayho3893@gmail.com>
2026-02-14 15:24:30 -0800

    test: create encapsulate naviagations and user actions for seach and opening cart

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -7,4 +7,25 @@ export class HomePage {
         await this.page.goto('/');
         await expect(this.page.getByRole('heading', { name: / sauce demo/i})).toBeVisible();
     }
+    
+    async openCatalog() {
+        await this.page.getByRole('link', {name: /catalog/i }).first().click();
+        await expect(this.page).toHaveURL(/\/collections\/all/i);
+    }
+    async search(query: string) {
+        await this.page.getByRole('link', {name: /search/i}).first().click();
+        await expect(this.page).toHaveURL(/\/search/i);
+
+        const searchInput = this.page.getByRole('textbox', {name: /search/i });
+        await expect(searchInput).toBeVisible();
+        await searchInput.fill(query);
+        await searchInput.press('Enter');
+
+        await expect(this.page).toHaveURL(/\/search/i);
+    }
+
+    async openCart() {
+        await this.page.goto('/cart');
+        await expect(this.page).toHaveURL(/\/cart/i);
+    }
 }
