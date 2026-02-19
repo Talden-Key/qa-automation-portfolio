586eec5332a72617f631f7b7c6109f2d32cdcd82
Talden-Key <tayho3893@gmail.com>
2026-02-18 20:52:46 -0800

    test: add cart api and clear cart api return empty

---
+++ tests/api/shopify-cart.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect } from "@playwright/test";
+
+test.describe("Shopify Ajax Cart API", () => {
+    test("GET /cart.js returns cart JSON", async ({ request }) => {
+        const  res = await request.get("/cart.js");
+        expect(res.status()).toBe(200);
+
+        const json = await res.json();
+        expect(json).toHaveProperty("items");
+        expect(json).toHaveProperty("item_count");
+        expect(Array.isArray(json.items)).toBeTruthy();
+    });
+
+    test("POST /cart/clear.js empties the cart", async({ request })=> {
+        const clearRes = await request.post("/cart/clear.js");
+        expect(clearRes.status()).toBe(200);
+
+        const cartRes = await request.get("/cart.js");
+        expect(cartRes.status()).toBe(200);
+
+        const cart = await cartRes.json();
+        expect(cart.item_count).toBe(0);
+        expect(cart.items.length).toBe(0);
+    });
+})
