513faf435b81369b832ee19983a7c5173f04765e
Talden-Key <tayho3893@gmail.com>
2026-02-26 20:37:54 -0800

    test: add item to cart with api

--- tests/api/cart.change-quantity.spec.ts
+++ tests/api/cart.change-quantity.spec.ts
@@ -8,5 +8,18 @@ test.describe("Shopify Ajax Cart API - change quantity", () => {
         // 2) Get a live variant id (no hardcoding)
         const productsRes = await request.get("/collections/all/products.json?limit=10");
         expect(productsRes.status(), "Expected produts.json to return 200").toBe(200);
+
+        const productsJson = await productsRes.json();
+        expect(Array.isArray(productsJson.products)).toBeTruthy();
+        expect(productsJson.products.length).toBeGreaterThan(0);
+
+        const firstVariantId: number = productsJson.products[0].variants[0].id;
+        expect(typeof firstVariantId).toBe("number");
+
+        // 3) add item to cart (quantity 1)
+        const addRes = await  request.post("/cart/add.js", {
+            form: { id: String(firstVariantId), quantity: "1"},
+        });
+        expect([200, 302]).toContain(addRes.status());
     })
 })
