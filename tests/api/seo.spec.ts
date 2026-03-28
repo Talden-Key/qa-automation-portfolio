05b0df318dd4090b07f018464561e08bc232ac89
Talden-Key <tayho3893@gmail.com>
2026-03-27 21:26:45 -0700

    test: added status check on robots page

--- tests/api/seo.spec.ts
+++ tests/api/seo.spec.ts
@@ -1,5 +1,11 @@
-import{test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("SEO endpoints (robots & sitemap)", () => {
-    
-})
+  test("GET /robots.txt returns 200 adn contains Sitemap directive", async ({
+    request,
+  }) => {
+    const res = await request.get("/robots.txt");
+
+    expect(res.status(), "Expected /robots.txt to return HTTP 200.").toBe(200);
+  });
+});
