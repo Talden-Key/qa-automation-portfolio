9e7227615e10fd113e3a3dcbdc5856c7a867afe1
Talden-Key <tayho3893@gmail.com>
2026-04-03 15:50:33 -0700

    test: ui search navigation

--- tests/ui/navigation.spec.ts
+++ tests/ui/navigation.spec.ts
@@ -2,6 +2,11 @@ import { test, expect} from '@playwright/test';
 
 test.describe("Navigation consistency", ()=> {
     test("header navigation routes to the expected pages", async ({ page }) => {
-        
+        await page.goto("/");
+
+        await page.getByRole("link", { name: /search/i }).first().click();
+        await expect(page).toHaveURL(/\search/i);
+        await expect(page.getByRole("textbox", { name: /search/i})).toBeVisible();
+
     })
 })
