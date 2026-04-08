3a2c266b500ed7aab42b6feaae7beb51fefb5429
Talden-Key <tayho3893@gmail.com>
2026-04-07 20:37:39 -0700

    test: created product page contract

---
+++ tests/ui/product.contract.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Product page contract", () => {
+    test("product detail page renders required elements", async ({ page }) => {
+        
+    })
+})
