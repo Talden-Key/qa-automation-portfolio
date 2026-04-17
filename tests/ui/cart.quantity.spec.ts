b2438981c0baa57cc735308b7ed84fd01400b738
Talden-Key <tayho3893@gmail.com>
2026-04-16 20:51:44 -0700

    test: go to catalog and open first product

--- tests/ui/cart.quantity.spec.ts
+++ tests/ui/cart.quantity.spec.ts
@@ -1,5 +1,14 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Cart quantity update", () => {
-    
+    test('update cart quantity from 1 to 2', async ({ page }) => {
+        await page.goto("/");
+
+        await page.getByRole("link", {name: /catalog/i }).first().click();
+        await expect(page).toHaveURL(/\/collections\/all/i);
+
+        const firstProduct = page.locator('a[href*="/products/"]').first();
+        await expect(firstProduct, "Expected at least one product link in catalog.").toBeVisible();
+        await firstProduct.click();
+    })
 })
